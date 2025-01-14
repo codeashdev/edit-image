e1be0ef9bc0027046deddb422d20036012c4b76b
codeashdev <ahmadshoaibt@hotmail.com>
2025-01-14 14:48:44 +0300

    adds active state to the nav items

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 export function MainNav({
 	className,
 	...props
 }: React.HTMLAttributes<HTMLElement>) {
+	const pathname = usePathname();
+
 	return (
 		<nav
 			className={cn("flex items-center space-x-4 lg:space-x-6", className)}
@@ -12,19 +17,37 @@ export function MainNav({
 		>
 			<Link
 				href="/"
-				className="text-sm font-medium transition-colors hover:text-primary"
+				className={cn(
+					"text-sm font-medium text-muted-foreground transition-colors",
+					{
+						"hover:text-primary": pathname !== "/",
+						"text-primary": pathname === "/",
+					},
+				)}
 			>
 				Home
 			</Link>
 			<Link
 				href="/dashboard"
-				className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+				className={cn(
+					"text-sm font-medium text-muted-foreground transition-colors",
+					{
+						"hover:text-primary": pathname !== "/dashboard",
+						"text-primary": pathname === "/dashboard",
+					},
+				)}
 			>
 				Dashboard
 			</Link>
 			<Link
 				href="/gallery"
-				className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+				className={cn(
+					"text-sm font-medium text-muted-foreground transition-colors",
+					{
+						"hover:text-primary": pathname !== "/gallery",
+						"text-primary": pathname === "/gallery",
+					},
+				)}
 			>
 				Gallery
 			</Link>
